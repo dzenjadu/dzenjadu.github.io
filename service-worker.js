6aafac39d5d4da25343dd677cfc2f4138276880f
dzenjadu <jaaadu@ya.ru>
2020-07-12 18:54:22 +0300

    Разработка wiki

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9785548ce3ba04d92c98eb240a4ce836.js"
+  "/precache-manifest.b3abb51a5954d1fe88a84ff7129eac5c.js"
 );
 
 self.addEventListener('message', (event) => {
