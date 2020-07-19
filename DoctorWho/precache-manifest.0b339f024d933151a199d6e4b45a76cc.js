0f8464793eb898e53b7480f6212dd6b2900334f0
dzenjadu <jaaadu@ya.ru>
2020-07-19 17:01:32 +0300

    Билдим приложение с нужными путями

---
+++ DoctorWho/precache-manifest.0b339f024d933151a199d6e4b45a76cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e37170f014a09a2011f261456f42b9c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "499263f7d53597482ed0",
+    "url": "/static/css/main.c7250998.chunk.css"
+  },
+  {
+    "revision": "398858272698f9bc7059",
+    "url": "/static/js/2.61607b86.chunk.js"
+  },
+  {
+    "revision": "0f2be52aac348a57b5e1722f475cbfd0",
+    "url": "/static/js/2.61607b86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "499263f7d53597482ed0",
+    "url": "/static/js/main.e796db94.chunk.js"
+  },
+  {
+    "revision": "19133a5d6c7be98533aa",
+    "url": "/static/js/runtime-main.107bb8ca.js"
+  }
+]);
